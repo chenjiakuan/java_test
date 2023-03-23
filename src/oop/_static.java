f371fd697dbdf3accfb61b5607a37ebc9b828b19
陈家宽 <861765484@qq.com>
2023-03-23 13:02:05 +0800

    static commit

--- src/oop/_static.java
+++ src/oop/_static.java
@@ -2,11 +2,15 @@ package oop;
 
 public class _static {
     public static void main(String[] args) {
-        Chinese c1=new Chinese(12,"cjk");
-        Chinese c2=new Chinese(13,"cjj");
-        System.out.println(Chinese.getCountry());
-        System.out.println(Chinese.country);
-        System.out.println(c1.country);
+//        Chinese c1=new Chinese(12,"cjk");
+//        Chinese c2=new Chinese(13,"cjj");
+//        System.out.println(Chinese.getCountry());
+//        System.out.println(Chinese.country);
+//        System.out.println(c1.country);
+        Account a1=new Account();
+        Account a2=new Account();
+        System.out.println(a1);
+        System.out.println(a2);
 
 
     }
@@ -45,5 +49,31 @@ class Chinese{
 
     public static void setCountry(String country) {
         Chinese.country = country;
+    }
+}
+
+class Account{
+    public int id;
+    public String pass;
+
+    private double balance;
+
+    private static double interestRate=0.3;
+    private static double minbalance=1;
+
+    private static int init=1001;
+    public Account(){
+        this.id=init;
+        init++;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", pass='" + pass + '\'' +
+                ", balance=" + balance +
+                '}';
     }
 }
