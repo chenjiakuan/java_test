333fd7b408e17a96921960bf513883fdb1e99f81
陈家宽 <861765484@qq.com>
2023-03-24 15:48:40 +0800

    afternoon commit

--- src/thread/thread_safe.java
+++ src/thread/thread_safe.java
@@ -2,8 +2,8 @@ package thread;
 
 class Sale implements Runnable {
 
-    int ticket = 100;
-    Object obj=new Object();
+    int ticket = 10000;
+    Object obj = new Object();
 
     //    synchronized
     @Override
@@ -11,7 +11,7 @@ class Sale implements Runnable {
         while (true) {
             synchronized (obj) {
                 if (ticket > 0) {
-                    System.out.println(Thread.currentThread().getName() + "卖出了一张票");
+                    System.out.print(Thread.currentThread().getName() + "卖出了一张票");
                     ticket -= 1;
                     System.out.println("此时还有" + ticket + "张票未卖出！");
 
@@ -24,9 +24,34 @@ class Sale implements Runnable {
     }
 }
 
+class Sale1 implements Runnable {
+
+    int ticket = 10000;
+    Object obj = new Object();
+    boolean isFlag = true;
+
+    //    synchronized
+    @Override
+    public void run() {
+        while (isFlag) {
+            show();
+        }
+    }
+
+    public synchronized void show() {
+        if (ticket > 0) {
+            System.out.print(Thread.currentThread().getName() + "卖出了一张票");
+            ticket -= 1;
+            System.out.println("此时还有" + ticket + "张票未卖出！");
+        } else {
+            isFlag = false;
+        }
+    }
+}
+
 public class thread_safe {
     public static void main(String[] args) {
-        Sale sale = new Sale();
+        Sale1 sale = new Sale1();
         Thread t1 = new Thread(sale);
         Thread t2 = new Thread(sale);
         Thread t3 = new Thread(sale);
