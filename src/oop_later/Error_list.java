f5f0e2094da4d8fefc2b9e137c963dff43fa498d
陈家宽 <861765484@qq.com>
2023-03-24 13:20:08 +0800

    afternoon commit

---
+++ src/oop_later/Error_list.java
@@ -0,0 +1,29 @@
+package oop_later;
+
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class Error_list {
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        try {
+//            int x = scanner.nextInt();
+//            int y = scanner.nextInt();
+//            System.out.println(x / y);
+//        }
+//
+//        catch (ArithmeticException e) {
+//            System.out.println("出现了分母为0的异常" + e);
+//            e.printStackTrace();
+//            e.getMessage();
+//        }
+//
+//
+//    }
+}
+
+
